683b34a7cd5d4328f787dd308c0ef70e08826b1f
Matt Cowles <selwocm@gmail.com>
2022-12-30 18:10:12 -0800

    create logic and styling to determine which card hand is the winner

--- src/Pokedex.js
+++ src/Pokedex.js
@@ -1,13 +1,22 @@
 import React, { Component } from "react"
-import pokemon from "./defaultPokemon"
 import Pokecard from "./Pokecard";
+import "./Pokedex.css"
 
 class Pokedex extends Component {
-    static defaultProps = { pokemon }
     render() {
+        let title;
+        if (this.props.isWinner) {
+            title = <h1 className="Poke-winner m-5">Winning Hand</h1>
+        } else {
+            title = <h1 className="Poke-loser m-5">Losing Hand</h1>
+        }
         return (
             <div>
-                <h1 className="mt-4 mb-5">Pokedex</h1>
+                <div className="d-flex justify-content-center align-items-center">
+                    <h1 className="m-5">Pokedex {this.props.dexNum}</h1>
+                    {title}
+                </div>
+                <p className="Pokedex-exp">Total Exp: {this.props.exp}</p>
                 <div className="d-flex justify-content-center">
                     {this.props.pokemon.map((p) => (
                         <Pokecard id={p.id} name={p.name} type={p.type} exp={p.base_experience} />
